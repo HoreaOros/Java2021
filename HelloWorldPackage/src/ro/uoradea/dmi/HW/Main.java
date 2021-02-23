2e8337ad4f33588b89959e3f59a3e52f7233d857
Horea Oros <horea.oros@gmail.com>
2021-02-23 13:29:25 +0200

    add simple projects

---
+++ HelloWorldPackage/src/ro/uoradea/dmi/HW/Main.java
@@ -0,0 +1,9 @@
+package ro.uoradea.dmi.HW;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world with packages!");
+    }
+}
